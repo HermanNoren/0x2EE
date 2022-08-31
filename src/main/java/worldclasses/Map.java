85ca663a58fda57bd98194667cd1a4bc177b8855
KingHermando <herman.noren@outlook.com>
2022-08-31 13:51:58 +0200

    Update Map.java

--- src/main/java/worldclasses/Map.java
+++ src/main/java/worldclasses/Map.java
@@ -1,6 +1,9 @@
 package worldclasses;
 
+import helperclasses.Rect;
 import helperclasses.Vector2;
+
+import java.util.ArrayList;
 
 public class Map {
 
@@ -27,9 +30,22 @@ public class Map {
             "                                        "
     };
 
+    public Map() {
+
+    }
+
+    public ArrayList<Tile> createMap() {
+        
+    }
 }
 
 class Tile {
     private Vector2 pos;
+    private Rect rect;
+
+    protected Tile(int x, int y) {
+        pos = new Vector2(x, y);
+        rect = new Rect(x, y, 16, 16);
+    }
 }
 
