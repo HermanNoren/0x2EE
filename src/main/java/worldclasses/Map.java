0d6aed65112d3c128e1459430351cddda005f031
KingHermando <herman.noren@outlook.com>
2022-08-31 13:31:10 +0200

    added a map

---
+++ src/main/java/worldclasses/Map.java
@@ -0,0 +1,35 @@
+package worldclasses;
+
+import helperclasses.Vector2;
+
+public class Map {
+
+    private String[] map = new String[] {
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "                                        ",
+            "             WWWWWWWWWWWW               ",
+            "                                        ",
+            "                                        "
+    };
+
+}
+
+class Tile {
+    private Vector2 pos;
+}
+
