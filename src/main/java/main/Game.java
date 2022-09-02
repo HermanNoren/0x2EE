0a8882d94e2b732338b2126bbaadf20b608eb37d
KingHermando <herman.noren@outlook.com>
2022-09-02 13:52:44 +0200

    name changes

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -6,7 +6,6 @@ import gamestates.InGameState;
 import sprites.Player;
 import sprites.Sprite;
 import view.Observer;
-import view.Window;
 
 import java.util.ArrayList;
 
@@ -104,9 +103,10 @@ public class Game implements Runnable {
     @Override
     public void run() {
 
-        double drawTime =  1000000000.0 / FPS;
-        double updateTime = 1000000000.0 / UPS;
-
+        double timePerRender =  1000000000.0 / FPS;
+        double timePerUpdate = 1000000000.0 / UPS;
+        double deltaUpdateTime = 0;
+        double deltaDrawTime = 0;
         long currentTime;
         long previousTime = System.nanoTime();
 
@@ -114,14 +114,11 @@ public class Game implements Runnable {
         int updates = 0;
         long lastFpsCheck = System.currentTimeMillis();
 
-        double deltaUpdateTime = 0;
-        double deltaDrawTime = 0;
-
         while(true) {
             currentTime = System.nanoTime();
 
-            deltaUpdateTime += (currentTime - previousTime) / updateTime;
-            deltaDrawTime += (currentTime - previousTime) / drawTime;
+            deltaUpdateTime += (currentTime - previousTime) / timePerUpdate;
+            deltaDrawTime += (currentTime - previousTime) / timePerRender;
             previousTime = currentTime;
 
             if (deltaUpdateTime >= 1) {
