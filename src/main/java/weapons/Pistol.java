2b7eddcbf8d79a4bd06eb04156894982dd99c76e
Kablero <kaslju@student.chalmers.se>
2022-09-06 11:00:27 +0200

    Fleshed Pistol out a bit (Gustav and Kasper)

--- src/main/java/weapons/Pistol.java
+++ src/main/java/weapons/Pistol.java
@@ -2,12 +2,14 @@ package weapons;
 
 public class Pistol implements Weapon{
 
-    int damage;
-    int ammo;
+    private int damage;
+    private int ammo;
+    private boolean reloading;
 
     public Pistol(){
         damage = 40;
         ammo = 7;
+        reloading = false;
     }
 
     @Override
@@ -18,8 +20,17 @@ public class Pistol implements Weapon{
 
     @Override
     public void reload(){
+        reloading = true;
+        // TODO: Add some kind of delay (might need threading?)
+        reloading = false;
         ammo = 7;
     }
+
+    /**
+     *
+     * @return true if weapon is reloading
+     */
+    public boolean isReloading(){return reloading;}
 
 
 }
