6cc35da16c00f718eb27ae958e9b0f3b0753f25b
Arthur <arthuralexandersson@hotmail.se>
2022-09-07 10:34:52 +0200

    pom file changes, and test test added

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sprites.Player;
+
+import java.io.IOException;
+
 public class PlayerTest {
+    Player player;
+    @BeforeEach
+    void init() throws IOException {
+        player = new Player(1, 1, 100); // Sets default starting values for player.
+    }
+    @Test
+    public void test_if_x_increases_when_d_is_pressed(){
+
+    }
+
+
 }
