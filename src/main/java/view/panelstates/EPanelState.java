d6aefd890b36db7ef8d4602185794a180ee1139f
Arthur <arthuralexandersson@hotmail.se>
2022-09-12 11:26:30 +0200

    Added enum for state

---
+++ src/main/java/view/panelstates/EPanelState.java
@@ -0,0 +1,14 @@
+package view.panelstates;
+
+/**
+ * Enum for all states in game. Add state if another state is implemeted.
+ */
+public enum EPanelState {
+    INGAME,
+    MAINMENU,
+    PAUSE,
+    HIGHSCORES,
+    GAMEOVER,
+    NEWHIGHSCORE,
+    SHOP;
+}
