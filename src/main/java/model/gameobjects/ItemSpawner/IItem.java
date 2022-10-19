f2827e77005e3a71df2ad5c99306b42c89c6372a
RLeksell <93666671+RLeksell@users.noreply.github.com>
2022-10-19 10:19:52 +0200

    Few Javadoc comment

--- src/main/java/model/gameobjects/ItemSpawner/IItem.java
+++ src/main/java/model/gameobjects/ItemSpawner/IItem.java
@@ -3,6 +3,9 @@ package model.gameobjects.ItemSpawner;
 import model.gameobjects.IGameObject;
 import model.gameobjects.Player;
 
+/**
+ * Interface every spawnable item should implement
+ */
 public interface IItem extends IGameObject {
     String getType();
     void consume(Player player);
